9a695601210cd2a6b9a9d77b53e2f4f5f2bd7805
July Bgdn <july.bgdn@gmail.com>
2017-11-09 08:57:05 +0300

    shiftNodeDown implemented, need to fix pop in Max-heap

--- src/max-heap.js
+++ src/max-heap.js
@@ -14,30 +14,57 @@ class MaxHeap {
 
 	pop() {
 		if(this.root !== null){
-
+			let temp = this.root;
+			let detach = this.detachRoot();
+			this.restoreRootFromLastInsertedNode(detach);
+			//this.shiftNodeDown(this.root);
+			return temp.data;
 		}
 		
 	}
 
 	detachRoot() {
-		
+        if(this.parentNodes.indexOf(this.root) !== -1)
+            this.parentNodes.shift();
+		let temp = this.root;
+		this.root = null;
+		return temp;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+		if ((detached.left !== null ||  detached.right !== null) && this.parentNodes.length > 0)   {
+            let temp = this.parentNodes.pop();
+            temp.left = detached.left;
+            temp.right = detached.right;
+            if(temp.left !== null)
+            	temp.left.parent = temp;
+            if(temp.right !== null)
+            	temp.right.parent = temp;
+            this.root = temp;
+            this.parentNodes.unshift(temp);
+		}
+
 	}
 
 	size() {
-        var count = 0;
+        let count = 0;
+        let index = 1;
         if (this.root !== null) {
-        	count = 1;
+        	count += index;
             while (this.root.right !== null) {
+            	index *= 2;
+                count += index;
                 this.root = this.root.right;
-                count += count*2;
             }
-
 		}
-		return count + this.parentNodes.length;
+		if (this.parentNodes.length < index) {
+            for (let i = 0; i < this.parentNodes.length; i++) {
+                if (this.parentNodes[i].left === null && this.parentNodes[i].right === null)
+                    count++;
+            }
+		}
+
+        return count;
 	}
 
 	isEmpty() {
@@ -80,7 +107,26 @@ class MaxHeap {
 	}
 
 	shiftNodeDown(node) {
-		
+        if (node.left !== null){
+            let temp1 = this.parentNodes.indexOf(node);
+            let temp2 = this.parentNodes.indexOf(node.left);
+            this.parentNodes[temp1] = node.left;
+            this.parentNodes[temp2] = node;
+            if (this.root === node)
+            	this.root = node.left;
+			node.left.swapWithParent();
+            this.shiftNodeDown(node);
+        }
+        else if(node.right !== null){
+            let temp1 = this.parentNodes.indexOf(node);
+            let temp2 = this.parentNodes.indexOf(node.right);
+            this.parentNodes[temp1] = node.right;
+            this.parentNodes[temp2] = node;
+            if (this.root === node)
+                this.root = node.right;
+            node.right.swapWithParent();
+            this.shiftNodeDown(node);
+        }
 	}
 }
 
