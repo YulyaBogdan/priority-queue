6e24635d903c04f48b6ed05bd4826fddf48a0a1c
July Bgdn <july.bgdn@gmail.com>
2017-11-11 00:00:58 +0300

    Queue implemented

--- src/queue.js
+++ src/queue.js
@@ -7,19 +7,24 @@ class PriorityQueue {
 	}
 
 	push(data, priority) {
-
+		if (this.maxSize <= this.heap.size())
+			throw Error;
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.isEmpty())
+			throw Error;
+		let temp = this.heap.pop();
+		return temp;
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		return this.heap.root === null;
+		return this.heap.isEmpty();
 	}
 }
 
